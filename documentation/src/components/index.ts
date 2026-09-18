7ad1efd7cf44556c81281800582e4f4246e67be9
vguerand <vguerand@users.noreply.github.com>
2026-09-18 05:21:39 +0000

    feat(docs): add DocHeaderSummary and interactive OnboardingTracks components
    Signed-off-by: vguerand <vguerand@users.noreply.github.com>

--- documentation/src/components/index.ts
+++ documentation/src/components/index.ts
@@ -1,8 +1,10 @@
 export * from "./Cards";
 export * from "./CodeTabs";
+export * from "./DocHeaderSummary";
 export * from "./DSFRPreviews";
 export * from "./Kanban";
 export * from "./LawSlashPreview";
 export * from "./Mermaid";
+export * from "./OnboardingTracks";
 export * from "./slash-preview";
 
